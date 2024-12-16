b677e1572a29c3cf29c50e347c0f782dd283580f
Wakoz <julien.cartron.pro@gmail.com>
2024-12-16 11:11:30 +0100

    perfect git commit -m'Finish'

--- src/main.tsx
+++ src/main.tsx
@@ -7,21 +7,63 @@ import App from "./App";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Article from "./pages/Article";
+
+const getWeatherOfTheDay = () => {
+  return "sunny";
+};
+
+const allData = [
+  {
+    id: 1,
+    title: "Lorem Ipsum",
+    content: "Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 2,
+    title: "Schnapsum",
+    content: "Lorem Elsass ipsum Salut bisamme",
+  },
+  {
+    id: 3,
+    title: "Cupcake Ipsum",
+    content: "Tiramisu pastry wafer brownie soufflé",
+  },
+];
+
+type Data = typeof allData[0];
+
+const getSomeData = (id: number) => {
+  return allData.find((article) => article.id === id) as Data | null;
+};
+
 
 // router creation
 
 const router = createBrowserRouter([
   {
     element: <App />,
+    loader: () => {
+      return getWeatherOfTheDay();
+    },
     children: [
       {
         path: "/",
         element: <Home />,
+        
       },
       {
         path: "/about",
         element: <About />,
       },
+      {
+        path: "/articles/:id",
+        element: <Article />,
+        loader: ({ params }) => {
+          const idAsInt = Number.parseInt(params.id ?? "0");
+          return getSomeData(idAsInt);
+      },
+    },
     ],
   },
 ]);
---
+++ src/pages/Article.tsx
@@ -0,0 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+
+type Data = {
+    title: string;
+    content: string;
+}
+
+function Article() {
+  const data = useLoaderData() as Data;
+
+  return (
+    <article>
+      <h1>{data.title}</h1>
+      <p>{data.content}</p>
+    </article>
+  );
+}
+
+export default Article;
